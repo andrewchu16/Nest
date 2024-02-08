c2e78556a3d8d0953efff5e475b6ec1f4492f243
Andrew Chu <67875282+andrewchu16@users.noreply.github.com>
2024-02-08 16:49:19 -0500

    firebase storage store

--- frontend/src/lib/auth.ts
+++ frontend/src/lib/auth.ts
@@ -1,14 +1,8 @@
 import { derived, type Readable } from 'svelte/store';
 import { browser, dev } from '$app/environment';
-import type { Auth, UserCredential } from 'firebase/auth';
+import type { Auth } from 'firebase/auth';
 import type { FirebaseApp } from 'firebase/app';
 import { app } from './app';
-
-// export interface AuthType extends Readable<Auth> {
-// 	signIn: (email: string, password: string) => Promise<UserCredential>;
-// 	signUp: (email: string, password: string) => Promise<UserCredential>;
-// 	signOut: () => Promise<void>;
-// }
 
 const createAuth = () => {
 	let auth: Auth;
