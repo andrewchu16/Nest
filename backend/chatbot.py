b91050860c53548b6a6d11c565867fbca0f6cfac
Andrew Chu <67875282+andrewchu16@users.noreply.github.com>
2024-02-25 11:13:32 -0500

    fixes

--- backend/chatbot.py
+++ backend/chatbot.py
@@ -2,17 +2,17 @@ import openai
 from flask import Flask, request, jsonify
 import gradio as gr
 
-openai.api_key = "####" #api here
+openai.apikey = "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" #api here
 
-messages = [{"role": "system", "content": "You are a technical support expert that specializes in helping users navigating the Nest website which helps organizations network with school departments"}]
+messages = [{"role": "system", "content": "At the beginning, say that your name is Nest help bot and You are a technical support expert that specializes in being the technical support for the company Nest which is responsible for school organizations networking with outside organizations the example prompts you should be able to output are (input e.g. How do I create an organization), (the output navigate to the my organizations tab, press the create new organization button then fill out the necessary information needed to create an organization"}]
 
-def chat_with_openai(user_input):
-    messages.append({"role": "user", "content": user_input})
+def chatwithopenai(userinput):
+    messages.append({"role": "user", "content": userinput})
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=messages
     )
-    ChatGPT_reply = response.choices[0].message.content
+    ChatGPTreply = response.choices[0].message.content
     messages.append({"role": "assistant", "content": ChatGPT_reply})
     return ChatGPT_reply
 
@@ -23,7 +23,7 @@ def CustomChatGPT(user_input):
     return chat_with_openai(user_input)
 
 #gradio app
-demo = gr.Interface(fn=CustomChatGPT, inputs="text", outputs="text", title="Nest Technical Support")
+demo = gr.Interface(fn=CustomChatGPT, inputs="text", outputs="text", title="Real Estate Pro")
 
 #endpoint for gradio interface to communicate with Flask
 @app.route('/api/chat', methods=['POST'])
@@ -33,11 +33,10 @@ def chat():
     chatbot_response = chat_with_openai(user_message)
     return jsonify({'response': chatbot_response})
 
-#gradio's `launch` method integrates with Flask
+#gradio's launch method integrates with Flask
 @app.route('/')
 def home():
     return demo.launch(share=True, server_name='0.0.0.0', server_port=5000, inline=True)
 
-if __name__ == '__main__':
+if __name == '__main':
     app.run(debug=True, port=5000)
-
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,19 +1,85 @@
 <script lang="ts">
-	import { onMount } from "svelte";
-	import Body from "$lib/components/Body.svelte";
+	import { onMount } from 'svelte';
+	import Body from '$lib/components/Body.svelte';
+	import type Organization from '$lib/models/organization.model';
+	import OrganizationCard from '$lib/components/OrganizationCard.svelte';
+	import { auth, db } from '$lib/firebase';
+	import { userStore } from '$lib/stores/userStore';
+	import { goto } from '$app/navigation';
+	import { collection, getDocs, query, where } from 'firebase/firestore';
+	import type { UserID } from '$lib/models/userData.model';
 
-    let loading = true;
+	let organizations: Organization[] = [];
 
-    onMount(() => {
-        loading = false;
-    });
+	userStore.subscribe(async (curr) => {
+		const orgsRef = collection(db, 'organizations');
+		const uid = curr.user?.uid ? curr.user?.uid : 'no';
+		const orgsQuery = query(orgsRef);
+
+		const orgsQuerySnap = await getDocs(orgsQuery);
+		orgsQuerySnap.forEach((doc) => {
+			const orgDocData = doc.data();
+			console.log(orgDocData);
+
+			const organizationData: Organization = {
+				name: '',
+				owners: [],
+				members: [],
+				thumbnailUrl: null,
+				description: null,
+				websiteUrl: null,
+				contactEmail: '',
+				industry: '',
+				sponsorRequirements: [],
+				type: null
+			};
+
+			organizationData.name = orgDocData['name'];
+			organizationData.owners = orgDocData['owners'] as UserID[];
+			organizationData.members = orgDocData['members'] as UserID[];
+			organizationData.thumbnailUrl = orgDocData['thumbnailUrl'];
+			organizationData.description = orgDocData['description'];
+			organizationData.websiteUrl = orgDocData['websiteUrl'];
+			organizationData.contactEmail = orgDocData['contactEmail'];
+			organizationData.industry = orgDocData['industry'];
+			organizationData.sponsorRequirements = orgDocData['sponsorRequirements'];
+			organizationData.type = orgDocData['type'];
+
+			if (!organizations.includes(organizationData)) {
+
+				organizations = [...organizations, organizationData];
+			}
+		});
+	});
+
+	const generateId = () => {
+		let result = '';
+		const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_';
+		for (let i = 0; i < 20; i++) {
+			result += characters.charAt(Math.floor(Math.random() * characters.length));
+		}
+		return result;
+	};
+
+	const redirectCreateNewOrganization = () => {
+		const orgId = generateId();
+
+		goto(`/organization/${orgId}/edit`);
+	};
 </script>
 
 <svelte:head>
-    <title>Dashboard</title>
+	<title>Dashboard</title>
 </svelte:head>
 <Body>
-    <svelte:fragment slot="page-content">
-        Dashboard
-    </svelte:fragment>
+	<svelte:fragment slot="page-content">
+		<div class="w-full h-full flex flex-col items-center px-8 py-12">
+			<h1 class="font-display font-bold text-3xl text-dark text-center">Dashboard</h1>
+			<div class="w-5/6 max-w-[1200px] h-full flex flex-col gap-4">
+				{#each organizations as organization}
+					<OrganizationCard {organization} />
+				{/each}
+			</div>
+		</div>
+	</svelte:fragment>
 </Body>
